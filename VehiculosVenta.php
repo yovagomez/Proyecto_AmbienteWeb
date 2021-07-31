8e1b92b26d7ab086e4336bac1b018860d810ccb2
D4nielSD <danielfsc2000@hotmail.com>
2021-07-30 18:08:28 -0600

    Cambios y añadidos generales

--- VehiculosVenta.php
+++ VehiculosVenta.php
@@ -31,8 +31,6 @@ CerrarBD($bdAbierta);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/css/mdb.min.css" rel="stylesheet">
     <link href="templates\estilo.css" rel="stylesheet">
     <title>Vehículos Venta FLIP-CARS</title>
@@ -41,24 +39,21 @@ CerrarBD($bdAbierta);
 <body>
     <!-- Bloque de codigo del NavBar/Barra del Menu -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark scrolling-navbar fixed-top">
-        <a class="navbar-brand" href="VehiculosVenta.php">FLIP CARS S.A</a>
+        <a class="navbar-brand" href="menu.php">FLIP CARS S.A</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">{Nombre de Usuario}</a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Gestión Vehiculos
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Vehiculos Alquiler</a>
+                        <a class="dropdown-item" href="VehiculosAlquiler.php">Vehiculos Alquiler</a>
                         <a class="dropdown-item" href="VehiculosVenta.php">Vehiculos Venta</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
@@ -77,8 +72,20 @@ CerrarBD($bdAbierta);
                     </div>
                 </li>
             </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">{Nombre de Usuario}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ajustes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Salir</a>
+                </li>
+            </ul>
         </div>
     </nav>
+    <!-- Fin del NavBar/Barra del Menu -->
 
     <table class="table">
         <thead class="thead-dark">
